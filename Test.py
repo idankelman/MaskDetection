b9bb97dec08c0e898908e339841b6a511e2b6060
idan kelman <kelmanidan@gmail.com>
2021-10-14 01:22:08 +0300

    Save Model

--- Test.py
+++ Test.py
@@ -1,6 +1,224 @@
-import cv2 
+
+import pandas as pd
 import numpy as np
+import cv2
+import os
+import glob
+from scipy.spatial import distance
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix
 
-img = cv2.imread('dataset\Data\images\maksssksksss0.png')
+import tensorflow as tf
+from tensorflow.keras import Sequential, models
+from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPool2D
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import model_from_json
 
-pass
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+
+
+
+#==============================================================================================
+#                                   Orgenizing the data
+#==============================================================================================
+
+image_path = 'images'
+
+path = "dataset/12kData/Face Mask Dataset/"
+
+dataset = {
+    "image_path": [],
+    "mask_status": [],
+    "where": []
+}
+
+for where in os.listdir(path):
+    for status in os.listdir(path+"/"+where):
+        for image in glob.glob(path+where+"/"+status+"/"+"*.png"):
+            dataset["image_path"].append(image)
+            dataset["mask_status"].append(status)
+            dataset["where"].append(where)
+            
+dataset = pd.DataFrame(dataset)
+dataset.head()
+
+
+
+#==============================================================================================
+#                                       Testing Masking 
+#==============================================================================================
+
+## Choosing a random image to detect the face in the image
+face_model = cv2.CascadeClassifier('dataset/DefaultXmls/haarcascade_frontalface_default.xml')
+
+## Choosing the image from the directory
+img = cv2.imread("dataset/Data/images/maksssksksss352.png")
+
+## Converting the image to grayscale to apply haarcascade algorithm
+img = cv2.cvtColor(img, cv2.IMREAD_GRAYSCALE)
+
+## Returns the x, y, w, h co-ordinates as numpy arrays for all the detected faces
+detected_face = face_model.detectMultiScale(img)
+
+## Converting from grayscale to colored image
+output_img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+## Drawing rectangle box around the faces in the image
+for (x, y, w, h) in detected_face:
+    cv2.rectangle(output_img, (x,y), (x+w, y+h), (0, 0, 200), 2)
+
+
+
+
+
+#==============================================================================================
+#                                   plotting the amount of data
+#==============================================================================================
+
+
+    
+## Displaying the image
+plt.figure(figsize = (15, 15))
+plt.imshow(output_img)
+plt.show()
+
+print(f"With Mask:", dataset.value_counts("mask_status")[0])
+print(f"Without Mask:", dataset.value_counts("mask_status")[1])
+
+## Plotting the numbers
+sns.countplot(x = dataset["mask_status"])
+
+
+
+#==============================================================================================
+#                                   plotting the data _ test
+#==============================================================================================
+
+plt.figure(figsize = (15, 10))
+
+for i in range(9):
+    random = np.random.randint(1, len(dataset))
+    plt.subplot(3, 3, i+1)
+    plt.imshow(cv2.imread(dataset.loc[random,"image_path"]))
+    plt.title(dataset.loc[random,"mask_status"], size = 15)
+    plt.xticks([])
+    plt.yticks([])
+    
+plt.show()
+
+
+
+
+#==============================================================================================
+#                                   splitting to train test and valid
+#==============================================================================================
+
+
+## Splitting train test and Validation Dataset
+train_df = dataset[dataset["where"] == "Train"]
+test_df = dataset[dataset["where"] == "Test"]
+valid_df = dataset[dataset["where"] == "Validation"]
+
+print(train_df.head(10))
+
+## Shuffling the dataset 
+train_df = train_df.sample(frac = 1)
+test_df = test_df.sample(frac = 1)
+valid_df = valid_df.sample(frac = 1)
+
+print("\n After Shuffling \n")
+print(train_df.head(10))
+
+
+
+
+#==============================================================================================
+# Reading all the image into a list and changing the size of the image to (150,150)
+#==============================================================================================
+
+train_df = train_df.reset_index().drop("index", axis = 1)
+train_df.head()
+
+
+data = []
+image_size = 150
+
+for i in range(len(train_df)):
+    ## Converting the image into grayscale
+    img_array = cv2.imread(train_df["image_path"][i], cv2.IMREAD_GRAYSCALE)
+
+    ## Resizing the array
+    new_image_array = cv2.resize(img_array, (image_size, image_size))
+
+    ##Encoding the image with the label
+    if train_df["mask_status"][i] == "WithMask":
+        data.append([new_image_array, 1])
+    else:
+        data.append([new_image_array, 0])
+
+
+data = np.array(data)
+data[0][0].shape
+
+
+## Shuffling the data to make sure everything is not in order
+np.random.shuffle(data)
+
+
+
+
+
+#==============================================================================================
+#               Preparing the data to be loaded into the model
+#==============================================================================================
+
+
+X = []
+y = []
+
+## Seperating X and y
+for image in data:
+    X.append(image[0])
+    y.append(image[1])
+    
+## Converting X and y to numpy array as Tensorflow accepts only numpy arrays
+X = np.array(X)
+y = np.array(y)
+### Normalizing the data
+X = X/255
+
+### Train Test Split
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+
+X_train = X_train.reshape(len(X_train), X_train.shape[1], X_train.shape[2], 1)
+X_val = X_val.reshape(len(X_val), X_val.shape[1], X_val.shape[2], 1)
+
+
+#==============================================================================================
+#                                   Building the model 
+#==============================================================================================
+
+
+
+
+# load json and create model
+json_file = open('Saved_Models/model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("Saved_Models/model.h5")
+print("Loaded model from disk")
+
+
+
+
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+score = loaded_model.evaluate(X_val, y_val, verbose=0)
+print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
