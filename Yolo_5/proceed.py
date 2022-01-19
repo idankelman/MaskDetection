7dc3143ffdc23387ddbb2040d01ae941cf98405f
idankelman <kelmanidan@gmail.com>
2022-01-19 17:55:33 +0200

    updated the proceed file

--- Yolo_5/proceed.py
+++ Yolo_5/proceed.py
@@ -2,3 +2,5 @@
 import os
 filename=input('please enter file name that you want to detect')
 os.system('python detect.py --weights best.pt --img 416 --conf 0.4 --source {}'.format(filename))
+#os.system('python detect.py --weights best.pt --img 416 --conf 0.4 --source 0')
+
