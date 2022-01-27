ba3379d03e745e58218859cb33d0da8dde6cedd2
idankelman <kelmanidan@gmail.com>
2022-01-27 19:09:54 +0200

    tested something .

--- Yolo_5/proceed.py
+++ Yolo_5/proceed.py
@@ -1,5 +1,10 @@
 
 import os
+import torch
+
+
+# print(torch.cuda.is_available())
+print(torch.__version__)
 filename=input('please enter file name that you want to detect')
 os.system('python detect.py --weights best.pt --img 416 --conf 0.4 --source {}'.format(filename))
 #os.system('python detect.py --weights best.pt --img 416 --conf 0.4 --source 0')
