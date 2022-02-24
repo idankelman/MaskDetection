e93ca4164733b913d53f5f9ff89154e7b12e87ac
idankelman <kelmanidan@gmail.com>
2022-02-24 15:27:51 +0200

    defnied the new websocket iterface

--- Django/Test/Test/asgi.py
+++ Django/Test/Test/asgi.py
@@ -7,10 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
-import os
+#===============================================================================
+#                Django Imports for the Web Server
+#===============================================================================
 
+import os
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+from channels.routing import URLRouter
+
+# from channels.auth import AuthMiddlewareStack
+# import Test.routing as routing
+# from Test.routing import ws_urlpatterns
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Test.settings')
 
-application = get_asgi_application()
+
+application =ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    # 'websocket':AuthMiddlewareStack(URLRouter(routing.ws_urlpatterns))
+})
+
+
