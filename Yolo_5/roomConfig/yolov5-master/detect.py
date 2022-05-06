985f73b10c6ce08fde2c93a587fb883f7298759e
Ofek <ofekzada@gmail.com>
2022-05-06 19:36:48 +0300

    added firebase configuration to project

--- Yolo_5/roomConfig/yolov5-master/detect.py
+++ Yolo_5/roomConfig/yolov5-master/detect.py
@@ -146,6 +146,8 @@ def run(
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             imc = im0.copy() if save_crop else im0  # for save_crop
             annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+            class_config = {}
+            print(det)
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
@@ -157,7 +159,7 @@ def run(
 
                 # Write results
                 # class name = 56 for chairs, run with --class 56
-                class_config = {}
+                
                 i=0
                 img = im0
                 for *xyxy, conf, cls in reversed(det):
@@ -189,6 +191,7 @@ def run(
                    
                     
             import collections
+            print(class_config)
             temp = collections.OrderedDict(sorted(class_config.items(), key=lambda t:t[1]["y0"]))
             chairs_ordered = {}
             row_num = 1
